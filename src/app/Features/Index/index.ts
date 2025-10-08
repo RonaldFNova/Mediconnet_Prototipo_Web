b2d72bf9f3091110670ab31eb659c22bf98e23fb
Ronald Bermudez <ronaldstevenrios@gmail.com>
2025-10-08 11:31:09 -0500

    Agregando estilos al index de la pagina

--- src/app/Features/Index/index.ts
+++ src/app/Features/Index/index.ts
@@ -2,16 +2,43 @@ import { Component } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { cookieService } from '../../Core/Service/cooke.service';
 import { tokenJwtService } from '../../Core/Service/tokenJwt.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-index',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './index.html',
   styleUrl: './index.css'
 })
 export class Index {
 
+    specialties = [
+    'Medicina General',
+    'Pediatría',
+    'Cardiología',
+    'Dermatología',
+    'Ginecología',
+    'Traumatología',
+    'Oftalmología',
+    'Odontología'
+  ];
+
   constructor(private router: Router, private cookieService: cookieService, private tokenjwtService: tokenJwtService) {}
+
+    scrollToEspecialidades() {
+    const element = document.getElementById('especialidades');
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+    scrollToServicios() {
+    const element = document.getElementById('servicios');
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+    scrollToContacto() {
+    const element = document.getElementById('contacto');
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 
   ngOnInit() {
