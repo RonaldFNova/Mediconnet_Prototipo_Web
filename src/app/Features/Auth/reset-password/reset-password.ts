0e30a1b2695e9f2061021ac2a6e97114711fac81
Ronald Bermudez <ronaldstevenrios@gmail.com>
2025-10-09 09:50:28 -0500

    Agregandole estilos a formulario de cambiar la contraseña

--- src/app/Features/Auth/reset-password/reset-password.ts
+++ src/app/Features/Auth/reset-password/reset-password.ts
@@ -3,10 +3,19 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { resetPassword } from '../../../Core/Service/resetPassword.service';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { Message } from 'primeng/message'
 
 @Component({
   selector: 'app-reset-password',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, 
+    ReactiveFormsModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    Message],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css'
 })
@@ -26,6 +35,7 @@ export class ResetPassword {
     const token = this.route.snapshot.queryParamMap.get('token') ?? "";
 
     if (!token) {
+      console.log(token)
       this.router.navigate(['index']);
       return;
     }
@@ -34,6 +44,13 @@ export class ResetPassword {
       this.mensaje = "Por favor complete el formulario";
       return;
     }
+
+    if (this.resetPasswordForm.value.confirmPassword =! this.resetPasswordForm.value.newPassword)
+    {
+      this.mensaje = "Contraseña no son iguales"
+      return;
+    }
+    
     console.log({Passwrod: this.resetPasswordForm.value.confirmPassword, Token: token});
 
     this.resetPassword.resetPassword({Password: this.resetPasswordForm.value.confirmPassword, Token: token}).subscribe({
